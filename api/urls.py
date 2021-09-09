7450f7269263fcf941af931d7297dd3a8908d4b8
Vikrom Narula <vikrom.nar@gmail.com>
2021-09-09 17:02:58 +0700

    Refactor, Added canReach()
    - Refactor API by remving all routes
    - Added canReach() for user to check if two station are reachable

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.apiOverview, name="api-overview"),
-    path("all_routes/", views.allRoutes, name="all-routes")
+    path("find_route/", views.findRoute, name="find_route"),
+    path("can_reach/", views.canReach, name="can-reach")
 ]
--- api/views.py
+++ api/views.py
@@ -1,21 +1,43 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import TrainSerializers
-from .models import Train
+from .graph import Graph
+
+graph = Graph()
+graph.createMapping()
 
 
 @api_view(['GET'])
-def apiOverview(req):
+def apiOverview(request):
     api_urls = {
         'Find Route': '/find_route',
-        'Can Reach': '/can_reach',
-        'All Routes': '/all_routes'
+        'Can Reach': '/can_reach'
     }
     return Response(api_urls)
 
 
 @api_view(['GET', 'POST'])
-def allRoutes(req):
-    train = Train.objects.all()
-    serialize = TrainSerializers(train, many=True)
-    return Response(serialize.data)
+def findRoute(request):
+    if request.method == 'GET':
+        api_info = {
+            "station_source": "Name of either MRT or BTS starting station i.e. Mo Chit",
+            "station_destination": "Name of either MRT or BTS destination station i.e. Ekkamai"
+        }
+        return Response(api_info)
+    elif request.method == 'POST':
+        data = request.data
+        path = graph.dfs(data["station_source"], data["station_destination"])
+        return Response(path if path != 1 else [])
+
+
+@api_view(['GET', 'POST'])
+def canReach(request):
+    if request.method == 'GET':
+        api_info = {
+            "station_source": "Name of either MRT or BTS starting station i.e. Mo Chit",
+            "station_destination": "Name of either MRT or BTS destination station i.e. Ekkamai"
+        }
+        return Response(api_info)
+    elif request.method == 'POST':
+        data = request.data
+        path = graph.dfs(data["station_source"], data["station_destination"])
+        return Response(path != -1)
