25e8c1ce6dd7d9a117aa41e1fef78c5ba7605978
Vikrom Narula <vikrom.nar@gmail.com>
2021-09-09 14:15:01 +0700

    Revert "Change model format"
    This reverts commit 58245530458091cb891ca8ddce5b15d0d86769db.

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.apiOverview, name="api-overview"),
-    path("all_routes/", views.allRoutes, name="all-routes")
+    path("", views.apiOverview, name="api-overview")
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,5 @@
-from .models import Train
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-from .serializers import StationSerializers, TrainSerializers, Train_to_StationSerializers
 
 
 @api_view(['GET'])
@@ -13,9 +10,3 @@ def apiOverview(req):
         'All Routes': '/all_routes'
     }
     return Response(api_urls)
-
-@api_view(['GET', 'POST'])
-def allRoutes(req):
-    train = Train.objects.all()
-    serializers = TrainSerializers(train)
-    return Response(serializers.data)
