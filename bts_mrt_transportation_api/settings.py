15644fabbd94896c6078de701b7e28173aa3a291
Vikrom Narula <vikrom.nar@gmail.com>
2021-09-09 11:13:21 +0700

    Initialize api app
    Created api app with `docker-compose run web python manage.py startapp api`

--- bts_mrt_transportation_api/settings.py
+++ bts_mrt_transportation_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['0.0.0.0']
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
