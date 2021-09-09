15b5cff59f91e9d11b56fbb732d1273b9ac8092d
Vikrom Narula <vikrom.nar@gmail.com>
2021-09-09 13:06:00 +0700

    Correctly register Model to admin

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Station, Train, Train_to_Station
 
-admin.register(Station)
-admin.register(Train)
-admin.register(Train_to_Station)
+admin.site.register(Station)
+admin.site.register(Train)
+admin.site.register(Train_to_Station)
 
