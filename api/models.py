2a0328ae88d7af5c712be9000a4c86703ceb1f9b
Vikrom Narula <vikrom.nar@gmail.com>
2021-09-09 11:19:12 +0700

    Created Database Model
    - Train_to_Station: Collection of train_id to its station_id
    - Station: Collection of station_name to its station_id
    - Train: Collection of train_id and its start and end station_id

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Train_to_Station(models.Model):
+    station_id = models.CharField(max_length=5)
+    train_id = models.CharField(max_length=5)
+
+    def __str__(self):
+        return "%s %s" % (self.station_id, self.train_id)
+
+
+class Station(models.Model):
+    station_id = models.CharField(max_length=5)
+    station_name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return "%s -> %s" % (self.station_id, self.station_name)
+
+
+class Train(models.Model):
+    train_id = models.CharField(max_length=5)
+    start_station_id = models.CharField(max_length=5)
+    end_station_id = models.CharField(max_length=5)
+
+    def __str__(self):
+        return "%s: %s -> %s" % (self.train_id, self.start_station_id, self.end_station_id)
