58245530458091cb891ca8ddce5b15d0d86769db
Vikrom Narula <vikrom.nar@gmail.com>
2021-09-09 13:58:25 +0700

    Change model format
    Due to problem with serializers will have to remove
     primary key out of the data

--- api/models.py
+++ api/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Train_to_Station(models.Model):
     station_id = models.CharField(max_length=5)
-    train_id = models.CharField(max_length=5, primary_key=True)
+    train_id = models.CharField(max_length=5)
 
     def __str__(self):
         return "%s %s" % (self.station_id, self.train_id)
 
 
 class Station(models.Model):
-    station_id = models.CharField(max_length=5, primary_key=True)
+    station_id = models.CharField(max_length=5)
     station_name = models.CharField(max_length=40)
 
     def __str__(self):
@@ -18,7 +18,7 @@ class Station(models.Model):
 
 
 class Train(models.Model):
-    train_id = models.CharField(max_length=5, primary_key=True)
+    train_id = models.CharField(max_length=5)
     start_station_id = models.CharField(max_length=5)
     end_station_id = models.CharField(max_length=5)
 
--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.apiOverview, name="api-overview")
+    path("", views.apiOverview, name="api-overview"),
+    path("all_routes/", views.allRoutes, name="all-routes")
 ]
