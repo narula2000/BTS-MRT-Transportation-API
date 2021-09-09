ec57efb0cb91d4a88428e4a41b656a4574364240
Vikrom Narula <vikrom.nar@gmail.com>
2021-09-09 13:23:26 +0700

    Set Up Restframework

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def apiOverview(req):
+    api_urls = {
+        'Find Route': '/find_route',
+        'Can Reach': '/can_reach',
+        'All Routes': '/all_routes'
+    }
+    return Response(api_urls)
