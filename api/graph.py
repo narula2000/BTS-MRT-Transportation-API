48f42b57e006da91614bac524b6d3d563feda361
Vikrom Narula <vikrom.nar@gmail.com>
2021-09-10 11:56:24 +0700

    Refactor Fucntion
    - Remove fucntion from public to internal function

--- api/graph.py
+++ api/graph.py
@@ -9,6 +9,11 @@ class Graph:
         self.createMapping()
 
     def createMapping(self):
+        def addEdge(current_node, next_node):
+            if current_node not in self.graph:
+                self.graph[current_node] = set()
+            self.graph[current_node].add(next_node)
+
         stations_links = Station_Link.objects.all()
         for station in stations_links.iterator():
             current_station_id = station.start_station_id
@@ -18,28 +23,22 @@ class Graph:
             next_station_id = station.end_station_id
             next_station_info = Station.objects.get(station_id=next_station_id)
             next_station_name = next_station_info.station_name
-            self.addEdge(current_station_name, next_station_name)
-            self.addEdge(next_station_name, current_station_name)
-
-    def addEdge(self, current_node, next_node):
-        if current_node not in self.graph:
-            self.graph[current_node] = set()
-        self.graph[current_node].add(next_node)
+            addEdge(current_station_name, next_station_name)
+            addEdge(next_station_name, current_station_name)
 
     def dfs(self, source_node, destination_node):
+        def dfsHelper(current_node, destination_node,
+                       visited_set, path_lst):
+            current_path = path_lst.copy()
+            visited_set.add(current_node)
+            current_path.append(current_node)
+            if current_node == destination_node:
+                self.path = current_path
+            for station in self.graph[current_node]:
+                if station not in visited_set:
+                    dfsHelper(station, destination_node,
+                                    visited_set, current_path)
         path_lst = []
         visited_set = set()
-        self.dfs_helper(source_node, destination_node, visited_set, path_lst)
+        dfsHelper(source_node, destination_node, visited_set, path_lst)
         return self.path
-
-    def dfs_helper(self, current_node, destination_node,
-                   visited_set, path_lst):
-        current_path = path_lst.copy()
-        visited_set.add(current_node)
-        current_path.append(current_node)
-        if current_node == destination_node:
-            self.path = current_path
-        for station in self.graph[current_node]:
-            if station not in visited_set:
-                self.dfs_helper(station, destination_node,
-                                visited_set, current_path)
