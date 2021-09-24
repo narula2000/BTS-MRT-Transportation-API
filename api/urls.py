447f8442c556e4173d9eb6940eb4197b2a6a41ed
Vikrom Narula <vikrom.nar@gmail.com>
2021-09-24 07:17:58 +0700

    Fix Error: Added mising / to path in urls

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("find_route_name/", views.findRouteByName, name="find-route-name"),
     path("find_route_id/", views.findRouteById, name="find-route-id"),
     path("get_station_name/", views.getStationNameById, name="get-station-name"),
-    path("get_station_id", views.getStationIdByName, name="get-station-id")
+    path("get_station_id/", views.getStationIdByName, name="get-station-id")
 ]
